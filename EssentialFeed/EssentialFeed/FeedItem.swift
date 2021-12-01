740ba7573ea7890bb824eec204b73af9b19df7ed
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2021-12-01 09:05:46 +0100

    Create FeedItem struct data model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Christophe Bugnon on 01/12/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let location: String?
+    let description: String?
+    let imageURL: URL
+}
